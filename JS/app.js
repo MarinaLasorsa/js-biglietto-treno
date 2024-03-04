53496a98bd2260ef18f51eef44f99f7c8a9fbe83
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-04 15:43:01 +0100

    ADD: description of all steps via comments

--- JS/app.js
+++ JS/app.js
@@ -5,3 +5,20 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 oppure applicato uno sconto del 40% per gli over 65.*/
 
+//1. determinare costanti numero di chilometri e età del passeggero tramite prompt
+
+//2. definire prezzo biglietto iniziale moltiplicando 0.21 per la costante chilometri dichiarata
+
+//3. SE costante età è minore di 18:
+//moltiplicare prezzo biglietto per 0.2
+//sottrarre risultato a prezzo biglietto
+//stampare risultato finale come prezzo finale del biglietto
+
+//ALTRIMENTI SE costante età è maggiore di 65:
+//moltiplicare prezzo biglietto per 0.4
+//sottrarre risultato a prezzo biglietto
+//stampare risultato finale come prezzo finale del biglietto
+
+//ALTRIMENTI 
+//sottrarre zero a prezzo biglietto
+//stampare risultato finale come prezzo finale del biglietto
